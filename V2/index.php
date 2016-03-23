2bffcbe5769d0ec0a2fc555c416dd0c0a9d6d2bc
ityler <tylernormile@gmail.com>
2016-03-22 22:08:05 -0400

    Demo updated
    Start of demo updates for landing page, non logged in

---
+++ V2/index.php
@@ -0,0 +1,76 @@
+<?php 
+$rootUrl = "/ts/";
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>TechScoopr - Home</title>
+  <!-- 
+  <link rel="stylesheet" href="http://techscoopr.com/assets/css/bootstrap.css" type="text/css">
+  <link rel="stylesheet" href="http://techscoopr.com/assets/css/bootstrap-responsive.min.css" type="text/css">
+  <link rel="stylesheet" href="http://techscoopr.com/assets/css/font-awesome.css" type="text/css">
+  <link rel="stylesheet" href="http://techscoopr.com/assets/css/custom-default.css" type="text/css">
+  <link rel="stylesheet" href="http://techscoopr.com/assets/fancybox/source/jquery.fancybox.css" type="text/css" media="screen"/>
+  <link rel="stylesheet" href="http://techscoopr.com/assets/fancybox/source/helpers/jquery.fancybox-buttons.css" type="text/css" media="screen"/>
+  <link rel="stylesheet" href="http://techscoopr.com/assets/fancybox/source/helpers/jquery.fancybox-thumbs.css" type="text/css" media="screen"/>
+  -->
+  <link rel="stylesheet" href="<?php echo $rootUrl ?>assets/css/bootstrap.min.css" type="text/css">
+  <link rel="stylesheet" href="<?php echo $rootUrl ?>assets/css/simple-sidebar.css" type="text/css">
+  <script src="<?php echo $rootUrl ?>assets/js/jquery-1.12.2.min.js"></script>
+  <script src="<?php echo $rootUrl ?>assets/js/masonry.pkgd.min.js"></script>
+  <script src="<?php echo $rootUrl ?>assets/js/bootstrap.min.js"></script>
+  <!--
+  <script src="http://techscoopr.com/assets/js/jquery-ui.min.js"></script>
+  <script src="http://techscoopr.com/assets/js/bootstrap.min.js"></script>
+  <script src="http://techscoopr.com/assets/js/modernizr-transitions.js"></script>
+  <script src="http://techscoopr.com/assets/fancybox/source/jquery.fancybox.pack.js"></script>
+  <script src="http://techscoopr.com/assets/fancybox/source/helpers/jquery.fancybox-buttons.js"></script>
+  <script src="http://techscoopr.com/assets/fancybox/source/helpers/jquery.fancybox-media.js"></script>
+  <script src="http://techscoopr.com/assets/fancybox/source/helpers/jquery.fancybox-thumbs.js"></script>
+  <script src="http://platform.twitter.com/widgets.js"></script>
+  <script src="http://techscoopr.com/assets/js/hammer.min.js"></script>
+  -->
+  <script type="text/javascript">
+  /*
+    var _gaq = _gaq || [];
+    _gaq.push(['_setAccount', 'UA-37368569-1']);
+    _gaq.push(['_trackPageview']);
+    (function() {
+      var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+      ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+      var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+    })();
+    */
+  </script> 
+</head>
+<body>
+  <div id="wrapper">
+    <!-- Sidebar -->
+    <div id="sidebar-wrapper">
+      <ul class="sidebar-nav">
+        <li class="sidebar-brand"><a href="#">TechScoopr</a></li>
+        <li><a href="#">Most Popular</a></li>
+        <li><a href="#">Latest News</a></li>
+        <li></li>
+        <li><a href="#">Amazon</a></li>
+        <li><a href="#">Apple</a></li>
+        <li><a href="#">Facebook</a></li>
+        <li><a href="#">Google</a></li>
+        <li><a href="#">Microsoft</a></li>
+        <li><a href="#">Samsung</a></li>
+      </ul>
+      <!-- 
+        # Landing page: Search keyword, return samples of news, add to "queue"
+        As latest news available, return news
+        # Keyword will be stored in temporary queue as category
+
+      -->
+    </div> 
+    <!-- /#sidebar-wrapper -->
+  </div>
+  <!-- /#wrapper -->
+</body>
+</html>
